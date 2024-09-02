963fbca674f4002476ee48f27f46dfd9965c9a93
dakshsaini05 <dakshsince2005@gmail.com>
2024-09-02 10:47:08 +0530

    Pattern Printing

--- 30Aug.c
+++ 30Aug.c
@@ -289,15 +289,15 @@ int main() {
     //   ***
     //    **
     //     *
-    // for(int i=1;i<=5;i++) {
-    //     for(int s=1;s<=5 - i;s++) {
-    //         printf(" ");
-    //     }
-    //     for(int j=5;j>=2 * i - 1;j--) {
-    //         printf("*");
-    //     }
-    //     printf("\n");
-    // }
+    for(int i=1;i<=5;i++) {
+        for(int s=1;s<=i;s++) {
+            printf(" ");
+        }
+        for(int j=5;j>=i;j--) {
+            printf("*");
+        }
+        printf("\n");
+    }
 
     // ####*
     // ###**
@@ -329,5 +329,194 @@ int main() {
         printf("\n");
     }
 
+//     *
+//    **
+//   ***
+//  ****
+// *****
+// *****
+//  ****
+//   ***
+//    **
+//     *
+    for(int i=1;i<=5;i++) {
+        for(int s=5;s>=i;s--) {
+            printf(" ");
+        }
+        for(int j=1;j<=i;j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int i=1;i<=5;i++) {
+        for(int s=1;s<=i;s++) {
+            printf(" ");
+        }
+        for(int j=5;j>=i;j--) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // *****
+    //  ****
+    //   ***
+    //    **
+    //     *
+    //     *
+    //    **
+    //   ***
+    //  ****
+    // *****
+    for(int i=1;i<=5;i++) {
+        for(int s=1;s<=i;s++) {
+            printf(" ");
+        }
+        for(int j=5;j>=i;j--) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int i=1;i<=5;i++) {
+        for(int s=5;s>=i;s--) {
+            printf(" ");
+        }
+        for(int j=1;j<=i;j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // *****
+    // #****
+    // ##***
+    // ###**
+    // ####*
+    for(int i=1;i<=5;i++) {
+        for(int s=1;s<=i;s++) {
+            printf("#");
+        }
+        for(int j=5;j>=i;j--) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // ####*
+    // ###**
+    // ##***
+    // #****
+    // *****
+    // *****
+    // #****
+    // ##***
+    // ###**
+    // ####*
+    for(int i=1;i<=5;i++) {
+        for(int s=5;s>=i;s--) {
+            printf("#");
+        }
+        for(int j=1;j<=i;j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int i=1;i<=5;i++) {
+        for(int s=1;s<=i;s++) {
+            printf("#");
+        }
+        for(int j=5;j>=i;j--) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    //     *
+    //    * *
+    //   * * * 
+    //  * * * * 
+    // * * * * * 
+    for(int i=1;i<=5;i++) {
+        for(int s=5;s>=i;s--) {
+            printf(" ");
+        }
+        for(int j=1;j<=i;j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    // * * * * * 
+    //  * * * *
+    //   * * *
+    //    * *
+    //     *
+    for(int i=1;i<=5;i++) {
+        for(int s=1;s<=i;s++) {
+            printf(" ");
+        }
+        for(int j=5;j>=i;j--) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    //     *
+    //    * *
+    //   * * * 
+    //  * * * * 
+    // * * * * * 
+    // * * * * * 
+    //  * * * *
+    //   * * *
+    //    * *
+    //     *
+    for(int i=1;i<=5;i++) {
+        for(int s=5;s>=i;s--) {
+            printf(" ");
+        }
+        for(int j=1;j<=i;j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for(int i=1;i<=5;i++) {
+        for(int s=1;s<=i;s++) {
+            printf(" ");
+        }
+        for(int j=5;j>=i;j--) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    // * * * * * 
+    //  * * * *
+    //   * * *
+    //    * *
+    //     *
+    //     *
+    //    * *
+    //   * * * 
+    //  * * * * 
+    // * * * * * 
+    for(int i=1;i<=5;i++) {
+        for(int s=1;s<=i;s++) {
+            printf(" ");
+        }
+        for(int j=5;j>=i;j--) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for(int i=1;i<=5;i++) {
+        for(int s=5;s>=i;s--) {
+            printf(" ");
+        }
+        for(int j=1;j<=i;j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
     return 0;
 }
